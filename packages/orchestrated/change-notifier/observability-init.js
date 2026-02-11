60dfd1a7385558e2099cc2fce7bb2ea13cb0ae44
Oli Sturm <oliver+commitsigning@oliversturm.com>
2026-02-11 20:59:53 +0000

    feat: observability

---
+++ packages/orchestrated/change-notifier/observability-init.js
@@ -0,0 +1,13 @@
+import { initialize } from '@lazyapps/observability';
+import { configureOtel } from '@lazyapps/logger';
+
+initialize({
+  serviceName: process.env.OTEL_SERVICE_NAME || 'change-notifier',
+  otlp: {
+    endpoint:
+      process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://localhost:4317',
+    protocol: process.env.OTEL_EXPORTER_OTLP_PROTOCOL || 'grpc',
+  },
+});
+
+configureOtel();
